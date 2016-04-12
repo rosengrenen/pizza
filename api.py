bdd27ac7eb5cd87a11e85d0b76800dbe80b727bd
hjorthjort <rikard.hjort@gmail.com>
2016-04-12 22:26:45 +0200

    add api for plaintext

--- api.py
+++ api.py
@@ -16,11 +16,29 @@ def json_pizza(pizza_args):
     ret += "}"
     return ret
 
+def html_pizza(pizza):
+    ret = "<h1>Name: " + pizza.name + "</h1>"
+    ret += "<ul>"
+    for i in pizza.ingredients:
+        ret += "<li>" + i + "</li>"
+    ret += "</ul>"
+    return ret
+
 @app.route("/")
+def html_veg_pizza():
+    pizza = random_pizza([])
+    return html_pizza(pizza)
+
+@app.route("/meat")
+def html_meat_pizza():
+    pizza = random_pizza(["meat"])
+    return html_pizza(pizza)
+
+@app.route("/json")
 def veg_pizza():
     return json_pizza([])
 
-@app.route("/meat")
+@app.route("/json/meat")
 def meat_pizza():
     return json_pizza(["meat"])
 
